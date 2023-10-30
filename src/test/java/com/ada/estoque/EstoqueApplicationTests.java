e02f6d12e4be92612ff5e8d2904d113a6f69c017
Milton <miltonmjcp@gmail.com>
2023-10-30 18:52:55 -0300

    chore: Base do projeto

---
+++ src/test/java/com/ada/estoque/EstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
